4f60f20c1d793b6820cfb1727f7a6892a9813baf
Daniel Zhu <dzhu2409@gmail.com>
2025-04-28 12:05:31 -0500

    Add global configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CustomConfigModule } from './global/config/config.module'
+import { DatabaseModule } from './global/database/database.module'
+import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 @Module({
-  imports: [],
+  imports: [CustomConfigModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ResponseInterceptor],
 })
 export class AppModule {}
