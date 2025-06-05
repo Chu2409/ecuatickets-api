6cb1f741b1846225819a9f24c44a69b182d3edd1
Zhu <dzhu2409@gmail.com>
2025-06-05 09:49:04 -0500

    Update verification mode to add company id and add decorator

--- src/core/auth/decorators/get-company-id.decorator.ts
+++ src/core/auth/decorators/get-company-id.decorator.ts
@@ -1,11 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 import { BaseUserResDto } from 'src/core/users/dto/res/user.dto'
 
-export const GetUser = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
+export const GetCompanyId = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest()
     const user = request.user as BaseUserResDto
 
-    return data ? user?.[data] : user
+    return user?.companyId
   },
 )
