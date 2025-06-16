180ada28f71e5063229a3a062799097acb2566a6
Zhu <dzhu2409@gmail.com>
2025-06-16 09:21:39 -0500

    Add bus to find alll route sheets

--- src/core/route-sheets/route-sheets.repository.ts
+++ src/core/route-sheets/route-sheets.repository.ts
@@ -108,6 +108,7 @@ export class RouteSheetsRepository {
               destination: true,
             },
           },
+          bus: true,
         },
         omit: {
           frequencyId: true,
