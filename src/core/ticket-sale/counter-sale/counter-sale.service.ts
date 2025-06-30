4c1d5dd72802b381b7f4c5066f84946fee7f044b
Gokochas10 <sebas.herrera3152@gmail.com>
2025-06-29 19:37:29 -0500

    include roles to endpoint pending transfers user and clerk

--- src/core/ticket-sale/counter-sale/counter-sale.service.ts
+++ src/core/ticket-sale/counter-sale/counter-sale.service.ts
@@ -23,7 +23,7 @@ export class CounterSalesService {
     private readonly emailService: EmailService,
     private readonly prisma: DatabaseService,
     private readonly paypalService: PayPalService,
-  ) { }
+  ) {}
 
   public async processCounterSale(dto: CreateCounterSaleDto) {
     const ticketsData = await this.prepareTicketsData(dto.tickets)
@@ -274,7 +274,8 @@ export class CounterSalesService {
   }
 
   public async getPendingTransferPayments() {
-    const payments = await this.ticketSaleRepository.findPendingTransferPayments()
+    const payments =
+      await this.ticketSaleRepository.findPendingTransferPayments()
 
     return {
       success: true,
